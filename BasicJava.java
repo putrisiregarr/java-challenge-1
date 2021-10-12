7b2970f7a9ecf36c43c8f5b96065bee98d51ab59
putrisiregarr <siregarrputri20@gmail.com>
2021-10-12 13:57:46 +0700

    first commit

---
+++ BasicJava.java
@@ -0,0 +1,34 @@
+public class BasicJava{
+    public static void main(String[] args) {
+    
+    //mendeklarasi variabel
+    String first, last, alamat, hp, hobi, makanan kesukaan;
+    int umur;
+    double berat, tinggi;
+    char goldar;
+
+    //mendeklarasi nilai
+    first = "Dwi Putri" ;
+    last = " Ramadanti Siregar";
+    alamat = "PematangSiantar";
+    hp = "081365054062";
+    hobi = "Baca Wattpad";
+    makanan kesukaan = "Mie Ayam"
+    umur = 21;
+    berat = 55;
+    tinggi = 160;
+    goldar = '0';
+
+    //menampilkan output
+    System.out.println("Nama ;" + first + last);
+    System.out.println("Alamat : " + alamat);
+    System.out.println("No hp :" + hp);
+    System.out.println("hobi :" + hobi);
+    System.out.println("makanan kesukaan :" + makanan kesukaan);
+    System.out.println("Umur :" + umur);
+    System.out.println("Berat :" + berat + "kg");
+    System.out.println("Tinggi :" + tinggi + "cm");
+    System.out.println("Golongan Darah :" + goldar);
+
+}
+}
